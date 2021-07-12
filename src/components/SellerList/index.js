bc2b03db968c6c628670e4605f0b4dd86ef175ec
GreatWorldDev <great.world.dev@gmail.com>
2021-07-13 01:54:28 +0900

    Move sellerlist css

--- src/components/SellerList/index.js
+++ src/components/SellerList/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './style.css';
 function SellerList(props) {
   const rank = props.index;
   const {image,nickName,currentPrice,verified} = props.data;
