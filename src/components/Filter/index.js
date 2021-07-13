6c7e0e3bfe4f00c1ea039d3a31b129228494ab35
GreatWorldDev <great.world.dev@gmail.com>
2021-07-13 21:44:07 +0900

    Create Activity Page and Activity Component

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,36 +1,43 @@
 import React from "react";
 import "./style.css";
-function Routes() {
-  return (
-    <div className="col-12">
-      <div className="main__filter">
-        <form action="#" className="main__filter-search">
-          <input type="text" placeholder="Search..."/>
-          <button type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"/></svg></button>
-        </form>
+function Filter() { 
+  return(
+    <div className="filter filter--sticky">
+      <h4 className="filter__title">Filters <button type="button">Clear all</button></h4>
 
-        <div className="main__filter-wrap">
-          <select className="main__select" name="status">
-            <option value="now">Buy Now</option>
-            <option value="new">New</option>
-            <option value="auction">On Auction</option>
-            <option value="offers">Has Offers</option>
-          </select>
-
-          <select className="main__select" name="category">
-            <option value="All genres">All categories</option>
-            <option value="1">Art</option>
-            <option value="2">Photography</option>
-            <option value="3">Games</option>
-            <option value="4">Metaverses</option>
-            <option value="5">Music</option>
-            <option value="6">Domains</option>
-            <option value="7">Memes</option>
-          </select>
-        </div>
+      <div className="filter__group">
+        <ul className="filter__checkboxes">
+          <li>
+            <input id="type5" type="checkbox" name="type5" defaultChecked/>
+            <label htmlFor="type5">Listings</label>
+          </li>
+          <li>
+            <input id="type6" type="checkbox" name="type6"/>
+            <label htmlFor="type6">Purchases</label>
+          </li>
+          <li>
+            <input id="type7" type="checkbox" name="type7" defaultChecked/>
+            <label htmlFor="type7">Sales</label>
+          </li>
+          <li>
+            <input id="type8" type="checkbox" name="type8" defaultChecked/>
+            <label htmlFor="type8">Transfers</label>
+          </li>
+          <li>
+            <input id="type9" type="checkbox" name="type9" />
+            <label htmlFor="type9">Bids</label>
+          </li>
+          <li>
+            <input id="type10" type="checkbox" name="type10" />
+            <label htmlFor="type10">Likes</label>
+          </li>
+          <li>
+            <input id="type11" type="checkbox" name="type11" />
+            <label htmlFor="type11">Followings</label>
+          </li>
+        </ul>
       </div>
     </div>
-  );
-}
-
-export default Routes;
+    );
+  }
+  export default Filter;
--- src/pages/Activity/index.js
+++ src/pages/Activity/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import BreadCrumb from "../../components/BreadCrumb";
-import Filter from "./Filter";
-import "./style.css";
+import Filter from "../../components/Filter";
+import Activity from "../../components/Activity";
 const breadcrumb = [
 	{title:"Home", page:'/'},
 	{title:"Activity", page:"/activity"},
+];
+const activityData=[
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"list", nickName:"@Nickname", bnbPrice:0.049, timeAgo:4},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"purchase", nickName:"@johndoe",bnbPrice:0.011, timeAgo:21},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"transfer", fromName:"@johndoe", toName:"@Nickname", timeAgo:21},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"offer",nickName:"@johndoe", bnbPrice:0.011, timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"like",nickName:"@johndoe", timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"start",nickName:"@johndoe", timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"list", nickName:"@Nickname", bnbPrice:0.049, timeAgo:4},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"purchase", nickName:"@johndoe",bnbPrice:0.011, timeAgo:21},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"transfer", fromName:"@johndoe", toName:"@Nickname", timeAgo:21},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"offer",nickName:"@johndoe", bnbPrice:0.011, timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"like",nickName:"@johndoe", timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"start",nickName:"@johndoe", timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"transfer", fromName:"@johndoe", toName:"@Nickname", timeAgo:21},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"offer",nickName:"@johndoe", bnbPrice:0.011, timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"like",nickName:"@johndoe", timeAgo:23},
+  {cover:"assets/img/cover/cover1.jpg", title:"Walking on Air", method:"start",nickName:"@johndoe", timeAgo:23}
 ];
 function Routes() {
   return (
@@ -35,226 +53,28 @@ function Routes() {
           {/* <!-- content --> */}
           <div className="col-12 col-xl-9 order-xl-1">
             <div className="row row--grid">
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover1.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Walking on Air</a></h3>
-                    <p className="activity__text">listed by <a href="author.html">@Nickname</a> <br/>for <b>0.049 ETH</b></p>
-                    <span className="activity__time">4 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover2.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">To Infinity And Beyond</a></h3>
-                    <p className="activity__text">9 editions listed by <a href="author.html">@neo</a> <br/>for <b>0.085 ETH</b> each</p>
-                    <span className="activity__time">21 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover3.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Flowers in Concrete</a></h3>
-                    <p className="activity__text">purchased by <a href="author.html">@johndoe</a> <b>0x23d2dc92b...82c6</b> for <b>0.011 ETH</b> from <a href="author.html">@min1max</a></p>
-                    <span className="activity__time">21 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover4.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Industrial Revolution</a></h3>
-                    <p className="activity__text">transferred from <a href="author.html">@midinh</a> <br/>to <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">23 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover5.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Tranquility</a></h3>
-                    <p className="activity__text"><a href="author.html">@aaarthur</a> offered <b>0.002 WETH</b></p>
-                    <span className="activity__time">41 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover6.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Tetradecapus</a></h3>
-                    <p className="activity__text">liked by <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">45 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover7.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Elegance</a></h3>
-                    <p className="activity__text">started following <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">48 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover8.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Walking on Air</a></h3>
-                    <p className="activity__text">started following <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">49 minutes ago</span>
-                  </div>
-                </div>
-              </div>
+              {activityData.map((activity,index)=>(
+                index < 8 &&
+                <div className="col-12 col-lg-6" key={`activity-${index}`}>
+                  <Activity data={activity}/>
+                </div>           
+              ))}           
             </div>
-
-            {/* <!-- collapse --> */}
             <div className="row row--grid collapse" id="collapsemore">
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover1.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Walking on Air</a></h3>
-                    <p className="activity__text">listed by <a href="author.html">@Nickname</a> <br/>for <b>0.049 ETH</b></p>
-                    <span className="activity__time">4 minutes ago</span>
-                  </div>
+              {activityData.map((activity,index)=>(
+              index >= 8 &&
+                <div className="col-12 col-lg-6">
+                  <Activity data={activity}/>
                 </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover2.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">To Infinity And Beyond</a></h3>
-                    <p className="activity__text">9 editions listed by <a href="author.html">@neo</a> <br/>for <b>0.085 ETH</b> each</p>
-                    <span className="activity__time">21 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover3.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Flowers in Concrete</a></h3>
-                    <p className="activity__text">purchased by <a href="author.html">@johndoe</a> <b>0x23d2dc92b...82c6</b> for <b>0.011 ETH</b> from <a href="author.html">@min1max</a></p>
-                    <span className="activity__time">21 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover4.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Industrial Revolution</a></h3>
-                    <p className="activity__text">transferred from <a href="author.html">@midinh</a> <br/>to <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">23 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover5.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Tranquility</a></h3>
-                    <p className="activity__text"><a href="author.html">@aaarthur</a> offered <b>0.002 WETH</b></p>
-                    <span className="activity__time">41 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover6.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Tetradecapus</a></h3>
-                    <p className="activity__text">liked by <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">45 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover7.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Elegance</a></h3>
-                    <p className="activity__text">started following <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">48 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-
-              <div className="col-12 col-lg-6">
-                <div className="activity">
-                  <a href="item.html" className="activity__cover">
-                    <img src="assets/img/cover/cover8.jpg" alt=""/>
-                  </a>
-                  <div className="activity__content">
-                    <h3 className="activity__title"><a href="item.html">Walking on Air</a></h3>
-                    <p className="activity__text">started following <a href="author.html">@johndoe</a></p>
-                    <span className="activity__time">49 minutes ago</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+              ))}  
+            </div>      
 
             <div className="row row--grid">
               <div className="col-12">
                 <button className="main__load" type="button" data-toggle="collapse" data-target="#collapsemore" aria-expanded="false" aria-controls="collapsemore">Load more</button>
               </div>
             </div>
-            {/* <!-- end collapse --> */}
           </div>
-          {/* <!-- end content --> */}
         </div>	
       </div>
     </main>
