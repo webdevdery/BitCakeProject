72c90824c0dc064a1068df9d0cbbdb6c283dcf1e
GreatWorldDev <great.world.dev@gmail.com>
2021-07-13 19:13:26 +0900

    Modify muliple image input in Card component

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,250 +1,121 @@
 import React from "react";
 import BreadCrumb from "../../components/BreadCrumb";
 import Filter from "../../components/Filter";
+import Card from "../../components/Card";
 const breadcrumb = [
 	{title:"Home",page:'/'},
 	{title:"Explorer",page:"/explorer"},
+];
+const cards = [
+  {
+    type:'image',
+    image: "assets/img/cover/cover1.jpg",
+    time: 900,
+    title: "Walking on Air",
+    avatar: "assets/img/avatars/avatar5.jpg",
+    nickName: "@nickname",
+    currentPrice: 4.89,
+    verified:true,
+    likes: 189,
+  },
+  {
+    type:'video',
+    image: ["assets/img/cover/cover3.jpg","assets/img/cover/cover3.jpg","assets/img/cover/cover3.jpg"],
+    time: 300,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:false,
+    likes: 37,
+  },
+  {
+    type:'image',
+    image: "assets/img/cover/cover2.jpg",
+    time: 3600,
+    title: "Les Immortels, the Treachery of Artificial Shadows",
+    avatar: "assets/img/avatars/avatar3.jpg",
+    nickName: "@neo",
+    currentPrice: 2.61,
+    verified:false,
+    likes: 702,
+  },
+  {
+    type:'image',
+    image: "assets/img/cover/cover3.jpg",
+    time: 300,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:true,
+    likes: 37,
+  },
+  {
+    type:'video',
+    image: "assets/img/cover/cover3.jpg",
+    time: 900,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:true,
+    likes: 37,
+  },
+  {
+    type:'image',
+    image: "assets/img/cover/cover3.jpg",
+    time: 3600,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:true,
+    likes: 37,
+  },
+  {
+    type:'video',
+    image: "assets/img/cover/cover3.jpg",
+    time: 300,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:false,
+    likes: 37,
+  },	
+  {
+    type:'video',
+    image: "assets/img/cover/cover3.jpg",
+    time: 300,
+    title: "Flowers in Concrete (Modal)",
+    avatar: "assets/img/avatars/avatar15.jpg",
+    nickName: "@min1max",
+    currentPrice: 3.19,
+    verified:false,
+    likes: 37,
+  },	
 ];
 function Explore() {
   return (
 		<main className="main">
 				<div className="container">
 					<div className="row row--grid">
-						{/* <!-- breadcrumb --> */}
 						<BreadCrumb data={breadcrumb}/>
-						{/* <!-- end breadcrumb --> */}
-
-						{/* <!-- title --> */}
 						<div className="col-12">
 							<div className="main__title main__title--page">
 								<h1>Explore exclusive digital assets</h1>
 							</div>
 						</div>
-						{/* <!-- end title --> */}
-
-						{/* <!-- filter --> */}
 						<Filter/>
-						{/* <!-- end filter --> */}
 					</div>
 
-					<div className="row row--grid">
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover">
-									<img src="assets/img/cover/cover1.jpg" alt=""/>
-								</a>
-								<h3 className="card__title"><a href="item.html">Walking on Air</a></h3>
-								<div className="card__author">
-									<img src="assets/img/avatars/avatar.jpg" alt=""/>
-									<a href="author.html">@nickname</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Reserve price</span>
-										<span>4.89 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>189</span>
-									</button>
-								</div>
+					<div className="row row--grid">						
+						{cards.map((card,index)=>(
+							<div className="col-12 col-sm-6 col-lg-4 col-xl-3"  key={`card-${index}`}>
+								<Card data={card}/>
 							</div>
-							{/* <!-- end card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- carousel card --> */}
-							<div className="card">
-								<div className="card__cover card__cover--carousel owl-carousel">
-									<img src="assets/img/cover/slide1.jpg" alt=""/>
-									<img src="assets/img/cover/slide2.jpg" alt=""/>
-									<img src="assets/img/cover/slide3.jpg" alt=""/>
-								</div>
-
-								<h3 className="card__title"><a href="item.html">To Infinity And Beyond</a></h3>
-								<div className="card__author card__author--verified">
-									<img src="assets/img/avatars/avatar2.jpg" alt=""/>
-									<a href="author.html">@johndoe</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Reserve price</span>
-										<span>5.04 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span></span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end carousel card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover">
-									<img src="assets/img/cover/cover2.jpg" alt=""/>
-									<span className="card__time card__time--clock">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M8.46777,8.39453l-.00225.00183-.00214.00208ZM18.42188,8.208a1.237,1.237,0,0,0-.23-.17481.99959.99959,0,0,0-1.39941.41114,5.78155,5.78155,0,0,1-1.398,1.77734,8.6636,8.6636,0,0,0,.1333-1.50977,8.71407,8.71407,0,0,0-4.40039-7.582,1.00009,1.00009,0,0,0-1.49121.80567A7.017,7.017,0,0,1,7.165,6.87793l-.23047.1875a8.51269,8.51269,0,0,0-1.9873,1.8623A8.98348,8.98348,0,0,0,8.60254,22.83594.99942.99942,0,0,0,9.98,21.91016a1.04987,1.04987,0,0,0-.0498-.3125,6.977,6.977,0,0,1-.18995-2.58106,9.004,9.004,0,0,0,4.3125,4.0166.997.997,0,0,0,.71534.03809A8.99474,8.99474,0,0,0,18.42188,8.208ZM14.51709,21.03906a6.964,6.964,0,0,1-3.57666-4.40234,8.90781,8.90781,0,0,1-.17969-.96387,1.00025,1.00025,0,0,0-.79931-.84473A.982.982,0,0,0,9.77,14.80957a.99955.99955,0,0,0-.8667.501,8.9586,8.9586,0,0,0-1.20557,4.71777,6.98547,6.98547,0,0,1-1.17529-9.86816,6.55463,6.55463,0,0,1,1.562-1.458.74507.74507,0,0,0,.07422-.05469s.29669-.24548.30683-.2511a8.96766,8.96766,0,0,0,2.89874-4.63269,6.73625,6.73625,0,0,1,1.38623,8.08789,1.00024,1.00024,0,0,0,1.18359,1.418,7.85568,7.85568,0,0,0,3.86231-2.6875,7.00072,7.00072,0,0,1-3.2793,10.457Z"/></svg>
-										<span className="card__clock card__clock--2"></span>
-									</span>
-								</a>
-								<h3 className="card__title"><a href="item.html">Les Immortels, the Treachery of Artificial Shadows</a></h3>
-								<div className="card__author card__author--verified">
-									<img src="assets/img/avatars/avatar3.jpg" alt=""/>
-									<a href="author.html">@neo</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Current price</span>
-										<span>2.61 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>702</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="#modal-asset" className="card__cover open-modal">
-									<img src="assets/img/cover/cover3.jpg" alt=""/>
-									<span className="card__time">an hour left</span>
-								</a>
-								<h3 className="card__title"><a href="#modal-asset" className="open-modal">Flowers in Concrete (Modal)</a></h3>
-								<div className="card__author">
-									<img src="assets/img/avatars/avatar4.jpg" alt=""/>
-									<a href="author.html">@min1max</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Current price</span>
-										<span>3.19 ETH</span>
-									</div>
-									
-									<button className="card__likes card__likes--active" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>37</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover">
-									<img src="assets/img/cover/cover4.jpg" alt=""/>
-								</a>
-								<h3 className="card__title"><a href="item.html">Industrial Revolution</a></h3>
-								<div className="card__author card__author--verified">
-									<img src="assets/img/avatars/avatar5.jpg" alt=""/>
-									<a href="author.html">@midinh</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Reserve price</span>
-										<span>1.11 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>23</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover">
-									<img src="assets/img/cover/cover5.jpg" alt=""/>
-									<span className="card__time">an hour left</span>
-								</a>
-								<h3 className="card__title"><a href="item.html">I Love You</a></h3>
-								<div className="card__author card__author--verified">
-									<img src="assets/img/avatars/avatar9.jpg" alt=""/>
-									<a href="author.html">@nicky87</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Current price</span>
-										<span>0.99 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>358</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- video card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover card__cover--video">
-									<img src="assets/img/cover/cover6.jpg" alt=""/>
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.53,7.15a1,1,0,0,0-1,0L17,8.89A3,3,0,0,0,14,6H5A3,3,0,0,0,2,9v6a3,3,0,0,0,3,3h9a3,3,0,0,0,3-2.89l3.56,1.78A1,1,0,0,0,21,17a1,1,0,0,0,.53-.15A1,1,0,0,0,22,16V8A1,1,0,0,0,21.53,7.15ZM15,15a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V9A1,1,0,0,1,5,8h9a1,1,0,0,1,1,1Zm5-.62-3-1.5V11.12l3-1.5Z"/></svg>
-								</a>
-								<h3 className="card__title"><a href="item.html">Walking on Air</a></h3>
-								<div className="card__author">
-									<img src="assets/img/avatars/avatar10.jpg" alt=""/>
-									<a href="author.html">@redalert</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Reserve price</span>
-										<span>1.63 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>90</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end video card --> */}
-						</div>
-
-						<div className="col-12 col-sm-6 col-lg-4 col-xl-3">
-							{/* <!-- card --> */}
-							<div className="card">
-								<a href="item.html" className="card__cover">
-									<img src="assets/img/cover/cover7.jpg" alt=""/>
-								</a>
-								<h3 className="card__title"><a href="item.html">Tranquility</a></h3>
-								<div className="card__author">
-									<img src="assets/img/avatars/avatar11.jpg" alt=""/>
-									<a href="author.html">@aaarthur</a>
-								</div>
-								<div className="card__info">
-									<div className="card__price">
-										<span>Reserve price</span>
-										<span>2.55 ETH</span>
-									</div>
-									
-									<button className="card__likes" type="button">
-										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.16,5A6.29,6.29,0,0,0,12,4.36a6.27,6.27,0,0,0-8.16,9.48l6.21,6.22a2.78,2.78,0,0,0,3.9,0l6.21-6.22A6.27,6.27,0,0,0,20.16,5Zm-1.41,7.46-6.21,6.21a.76.76,0,0,1-1.08,0L5.25,12.43a4.29,4.29,0,0,1,0-6,4.27,4.27,0,0,1,6,0,1,1,0,0,0,1.42,0,4.27,4.27,0,0,1,6,0A4.29,4.29,0,0,1,18.75,12.43Z"/></svg>
-										<span>64</span>
-									</button>
-								</div>
-							</div>
-							{/* <!-- end card --> */}
-						</div>
+						))} 						
 					</div>
 
 					{/* <!-- paginator --> */}
