9352827dc532874e89367d3030867ce6ccb15fac
jonathan <crypto.phantomguru@gmail.com>
2021-07-16 01:50:47 +0800

    Overflow fix

--- src/components/Card/AudioImage/index.js
+++ src/components/Card/AudioImage/index.js
@@ -1,16 +1,19 @@
-
-import React from 'react'
-import Waveform from './Waveform'
+import React from "react";
+import Waveform from "./Waveform";
 import "./style.css";
 export default function AudioImage(props) {
   return (
-      <div>
-        <img alt="" src={props.src} className="w-full h-80" />
-        <div className="absolute bottom-8 w-full">
-          <Waveform audioURL={props.audioPath} onClick={props.onClick}/>
-        </div>
+    <div>
+      <img alt="" src={props.src} className="w-full h-80" />
+      <div className="absolute bottom-8 w-full">
+        <Waveform
+          audioURL={props.audioPath}
+          onClick={props.onClick}
+          style={{ overflow: "hidden!important" }}
+        />
       </div>
-  )
+    </div>
+  );
 }
 
 // import React, { useState } from 'react';
